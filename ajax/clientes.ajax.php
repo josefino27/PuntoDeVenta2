912c015080a0dd0568eefbb8f37c8ac973f6ad02
josefino27 <jodanu19@gmail.com>
2024-05-31 21:04:45 -0500

    first commit

---
+++ ajax/clientes.ajax.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once "../controladores/clientes.controlador.php";
+require_once "../modelos/clientes.modelo.php";
+
+class AjaxClientes{
+
+	/*=============================================
+	EDITAR CLIENTE
+	=============================================*/	
+
+	public $idCliente;
+
+	public function ajaxEditarCliente(){
+
+		$item = "id";
+		$valor = $this->idCliente;
+
+		$respuesta = ControladorClientes::ctrMostrarClientes($item, $valor);
+
+		echo json_encode($respuesta);
+
+
+	}
+	public function ajaxActualizarCliente(){
+
+		$item = "id";
+		$valor = $this->idCliente;
+
+		$respuesta = ControladorClientes::ctrMostrarClientes($item, $valor);
+
+		echo json_encode($respuesta);
+
+
+	}
+
+}
+
+/*=============================================
+EDITAR CLIENTE
+=============================================*/	
+
+if(isset($_POST["idCliente"])){
+
+	$cliente = new AjaxClientes();
+	$cliente -> idCliente = $_POST["idCliente"];
+	$cliente -> ajaxEditarCliente();
+
+}
+
+/*=============================================
+ACTUALIZAR CLIENTE - COMPRA GRATIS
+=============================================*/	
+
+if(isset($_POST["clienteFree"])){
+
+	$cliente = new AjaxClientes();
+	$cliente -> idCliente = $_POST["clienteFree"];
+	$cliente -> ajaxActualizarCliente();
+
+}
+
